345a805dda9b329326e179f1a3a2f731b948300f
Kumo Tyou <zy.hua1122@qq.com>
2019-02-14 22:56:37 +0800

    dev

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material';
 
 import { MaterialModule } from './modules/material.module';
 import { MarkdownModule } from './modules/markdown.module';
@@ -25,6 +26,7 @@ import { LoadingPlaceholderComponent } from './components/loading-placeholder/lo
     FormsModule,
     ReactiveFormsModule,
     LoadingPlaceholderComponent
-  ]
+  ],
+  providers: [{ provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2000 } }]
 })
 export class SharedModule {}
